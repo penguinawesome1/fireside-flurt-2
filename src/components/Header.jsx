d8461e1f21ec6763cf31e176cd849e6704f6a250
Owen Colley <owen.h.colley@gmail.com>
2024-12-21 14:42:53 -0500

    hi

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,10 @@ function Header() {
         <>
             <header>
                 <nav className="bg-rose-300 py-2 px-6 fixed w-[100%] flex justify-between items-center rounded-b-2xl">
-                    <a href="index.html"><img src="images/logo.png" className="w-10 h-10" alt="logo" /></a>
+                    <a href="index.html"><img src="./src/images/logo.png" className="w-10 h-10" alt="logo" /></a>
                     <div className="flex gap-4">
-                        <a className="bg-white p-2 rounded-xl border-solid hover:border-dashed border-2 border-zinc-800" href="html/sign-in.html">Log In</a>
-                        <a className="bg-white p-2 rounded-xl border-solid hover:border-dashed border-2 border-zinc-800" href="html/sign-up.html">Sign Up</a>
+                        <a className="bg-white p-2 rounded-xl border-solid hover:border-dashed border-2 border-zinc-800" href="public/sign-in.html">Log In</a>
+                        <a className="bg-white p-2 rounded-xl border-solid hover:border-dashed border-2 border-zinc-800" href="public/sign-up.html">Sign Up</a>
                     </div>
                 </nav>
             </header>
