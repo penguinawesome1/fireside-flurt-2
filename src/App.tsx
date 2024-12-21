029ec50435393e33b1732ca1aec5e9febaeccfa6
Owen Colley <owen.h.colley@gmail.com>
2024-12-21 16:17:55 -0500

    did cool react stuff with mapping to components

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import Header from './components/Header.tsx';
 import Card from './components/Card.tsx';
 
 function App() {
+	const names = ["John Doe", "Name 2", "John Smith", "Jane Smith", "John Johnson", "Jane Johnson", "John Williams", "Jane Williams", "last"];
+	const urls = ["./images/1.jpg", "./images/2.jpg", "./images/3.jpg", "./images/4.jpg", "./images/5.jpg", "./images/6.jpg", "./images/7.jpg", "./images/8.jpg", "./images/9.jpg"];
+	const descriptions = ["First description", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec odio nec urna t", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec odio nec urna t", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec odio nec urna t", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec odio nec urna t", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec odio nec urna t", "Hola", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec odio nec urna t", "last"];
+
 	return (
 		<>
 			<Header />
 			<div className="w-[100vw] h-[100svh] flex justify-start items-center gap-12 overflow-x-scroll snap-x snap-proximity px-[40%]">
-				<Card />
-				<Card />
-				<Card />
-				<Card />
+				{names.map((name, i) => (
+					<Card name={name} url={urls[i]} description={descriptions[i]} />
+				))}
 			</div>
 		</>
 	);
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,10 +1,10 @@
-function Card() {
+function Card({ name, url, description }) {
     return (
         <>
             <div className="w-96 aspect-square text-wrap flex flex-col justify-between items-center bg-emerald-400 rounded-3xl relative flex-shrink-0 snap-center">
-                <h1 className="text-5xl font-semi-bold absolute drop-shadow-[0_1.2px_1.2px_rgb(255,255,255)]">User 1</h1>
-                <img className="border-solid hover:border-dashed border-2 border-zinc-800 rounded-3xl w-[100%] h-[70%] bg-white" src="" alt="profile image" />
-                <p className="p-4 h-[30%] text-md flex items-center">Lorem ipsum dolor tur adipisicing elit. Nequr tur adipisicing elit.</p>
+                <h1 className="text-5xl font-semi-bold absolute drop-shadow-[0_1.2px_1.2px_rgb(255,255,255)]">{name}</h1>
+                <img className="border-solid hover:border-dashed border-2 border-zinc-800 rounded-3xl w-[100%] h-[70%] bg-white" src={url} alt="profile image" />
+                <p className="p-4 h-[30%] text-md flex items-center">{description}</p>
             </div>
         </>
     );
